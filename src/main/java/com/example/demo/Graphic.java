fa992284d635689f5795423d1389748f96829a0e
chihiro.nakamura <chihiro.moment.1127@gmail.com>
2023-02-16 20:41:34 +0900

    create graphic

--- src/main/java/com/example/demo/Graphic.java
+++ src/main/java/com/example/demo/Graphic.java
@@ -3,8 +3,17 @@ package com.example.demo;
 public class Graphic {
 
 	public static void main(String[] args) {
-		
+		System.out.println( "hellow world" );
 
+		javax.swing.JFrame jf = new javax.swing.JFrame();
+		jf.setBounds( 20, 30, 400, 400); // ウィンドウの大きさ
+		jf.setVisible(true);
+		jf.setDefaultCloseOperation( javax.swing.JFrame.EXIT_ON_CLOSE );
+		
+		javax.swing.JLabel jl = new javax.swing.JLabel( "hellow world" );
+		jl.setFont( new java.awt.Font( null, 0, 30 ) );	// 文字の大きさ
+		jl.setForeground( java.awt.Color.RED ); // 文字の色
+		jf.add( jl );
 	}
 
 }
